6cb6213c11dc21203323315dca43d6938efe27c6
lukasrueda <62038091+lukasrueda@users.noreply.github.com>
2020-04-27 18:15:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import datetime
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # configuring the connection to postgresql database
 # app.config['DATABASE_URL'] = 'postgres://hzimzwnodcjnlg:b7d3f5287994c904922a28b91a35f48b9ca966791bc454398bc6412fb214d620@ec2-52-6-143-153.compute-1.amazonaws.com:5432/d3kb2tqikr63h8'
